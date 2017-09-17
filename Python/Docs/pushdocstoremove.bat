50ec3b5c6ed7ec731caa927e71a37a6a6e66d67b
Thore Goll <thore.goll@tum.de>
2017-09-17 15:05:07 +0200

    added doc build script to push to branch gh-pages

---
+++ Python/Docs/pushdocstoremove.bat
@@ -0,0 +1,25 @@
+@echo off
+
+SET ROOT="%~dp0\.."
+
+REM build the docs
+cd %ROOT%\Python\Docs
+make clean
+make html
+xcopy /s build "%TEMP%\build\"
+cd %ROOT%
+
+REM switch branches and pull the data we want
+git checkout gh-pages
+cd ..
+del /Q %ROOT%\*
+for /d %x in (%ROOT%\*) do @rd /S /Q "%x"
+cd %ROOT%
+copy NUL .nojekyll
+xcopy /e /h /y /c "%TEMP%\build\html" .\
+git add -A
+git commit -m "publishing updated docs..."
+git push origin gh-pages
+
+REM switch back
+git checkout master
