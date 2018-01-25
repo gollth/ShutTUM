bbe04525d8008226ebe689cb67794ddc577d8d3a
Thore Goll <thore.goll@tum.de>
2018-01-25 10:11:06 +0100

    add optional camera model

--- Examples/ros/src/stereotum/scripts/kalibr_calibrate_cameras.bash
+++ Examples/ros/src/stereotum/scripts/kalibr_calibrate_cameras.bash
@@ -2,5 +2,5 @@
 rosrun kalibr kalibr_calibrate_cameras \
 	--bag $1 \
 	--topics /cam/global/left/image_raw/decimated /cam/global/right/image_raw/decimated /cam/rolling/left/image_raw/decimated /cam/rolling/right/image_raw/decimated \
-	--models pinhole-fov pinhole-fov pinhole-fov pinhole-fov \
+	--models pinhole-$3 pinhole-$3 pinhole-$3 pinhole-$3 \
 	--target $2
